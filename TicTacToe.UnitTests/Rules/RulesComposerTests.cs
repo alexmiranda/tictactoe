2752005f5bde177da0d5d3f7f11947656ff401a1
alexmiranda <alexmirandamoraes@gmail.com>
2013-01-22 23:40:32 -0200

    Adds RulesComposer class and tests

---
+++ TicTacToe.UnitTests/Rules/RulesComposerTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using TicTacToe.Games;
+using TicTacToe.Games.Grid;
+using TicTacToe.Games.Grid.ThreePerThree;
+using TicTacToe.Games.Rules;
+
+namespace TicTacToe.UnitTests.Rules
+{
+    [TestFixture]
+    public class RulesComposerTests
+    {
+        private RulesComposer _composer;
+        private Mock<IGame> _gameMock;
+
+        [SetUp]
+        public void CanCreateRulesComposer()
+        {
+            _gameMock = new Mock<IGame>();
+            _composer = new RulesComposer();
+            _composer.SetGame(_gameMock.Object);
+        }
+
+        [Test]
+        public void CanAddAssistant()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                var assistantMock = new Mock<IRuleAssistant>();
+                _composer.Add(assistantMock.Object);
+            });
+        }
+
+        [Test]
+        public void AcceptMove_ShouldCallAllAssistants()
+        {
+            var assistantMock = new Mock<IRuleAssistant>();
+            assistantMock.Setup(x => x.AcceptMove(It.IsAny<Move>()))
+                .Verifiable();
+
+            _composer.Add(assistantMock.Object);
+
+            var move = new Move(Mark.Cross, Positions3X3.Center);
+            _composer.AcceptMove(move);
+
+            assistantMock.Verify(x => x.AcceptMove(move), Times.Once());
+        }
+    }
+}
---
+++ TicTacToe/Games/Rules/RulesComposer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace TicTacToe.Games.Rules
+{
+    public class RulesComposer : RuleAssistant
+    {
+        private readonly ISet<IRuleAssistant> _assistants = new HashSet<IRuleAssistant>();
+
+        public virtual void Add(IRuleAssistant assistant)
+        {
+            _assistants.Add(assistant);
+        }
+
+        public override void AcceptMove(Move move)
+        {
+            foreach (var assistant in _assistants)
+                assistant.AcceptMove(move);
+        }
+    }
+}
